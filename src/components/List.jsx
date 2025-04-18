f9b18ef11a88f5fdc14803fb3a6b2ab183b3f172
sinyeongseo <jasmine88368@metanetglobal.com>
2025-04-18 10:15:17 +0900

    Creat Read - 투두 리스트 렌더링하기

---
+++ src/components/List.jsx
@@ -0,0 +1,39 @@
+import "./List.css"
+import TodoItem from "./TodoItem";
+import {useState} from 'react'
+
+const List = ({todos}) => { 
+
+    const [search ,setSearch] = useState("");
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    };
+
+    const getFilteredData = () => {
+        if(search === ""){
+            return todos;
+        }
+        return todos.filter(
+            (todo) => todo.content.toLowerCase().includes(search.toLowerCase())
+        );
+    }
+
+    const filteredTodos = getFilteredData();
+
+    return (
+        <div className="List">
+            <h1>Todo List🎄</h1>
+            <input value={search} onChange ={onChangeSearch} placeholder="검색어를 입력하세요"></input>
+            <div className="todos_wrapper">
+                {filteredTodos.map((todo) => {
+                    return <TodoItem key={todo.id} {...todo} /> ;
+                })}
+            </div>
+        </div>
+        
+    );
+    
+}
+
+export default List;
